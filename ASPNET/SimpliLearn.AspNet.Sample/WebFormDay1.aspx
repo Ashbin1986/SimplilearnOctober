89866d064de8987eacf1496b83068de7ae40bbcf
Ashbin1986 <ashbin.kumar01@gmail.com>
2021-10-27 10:04:51 +0530

    Asp.Net Session
    Day 1

---
+++ ASPNET/SimpliLearn.AspNet.Sample/WebFormDay1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormDay1.aspx.cs" Inherits="SimpliLearn.AspNet.Sample.WebFormDay1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+<div>
+    File Upload : <asp:FileUpload ID="fileuploadImage" runat="server" />
+    <asp:Button ID="btnSave" runat="server" Text="Save" BackColor="#FF9966" OnClick="btnSave_Click" />
+    <asp:Image ID="img" runat="server" />
+    <br />
+    <br />
+    <asp:Label ID="lblError" runat="server" ></asp:Label>
+</div>
+    </form>
+</body>
+</html>
---
+++ ASPNET/SimpliLearn.AspNet.Sample/WebFormDay1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpliLearn.AspNet.Sample
+{
+    public partial class WebFormDay1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            if(fileuploadImage.HasFile)
+            {
+                fileuploadImage.SaveAs(Server.MapPath("~/Files/" + fileuploadImage.FileName));
+                lblError.Text = "File uploaded successfully !.";
+                img.ImageUrl = Server.MapPath("~/Files/" + fileuploadImage.FileName);
+            }
+            else
+            {
+                lblError.Text = "File not selected !.";
+            }
+        }
+    }
+
+}
